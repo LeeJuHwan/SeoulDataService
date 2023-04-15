3ed3a4d8dcf013a54047b994a1dc3d2b46eb113f
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-04-15 21:26:59 +0900

    [ADD] web base MVP pattern

---
+++ backend/Opendata/web/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = "web"
+
+urlpatterns = [
+    path("", views.MainView.as_view(), name = "main"),
+    # path("car/", views.CarFormView.as_view(), name = "car"),
+    path("create_car/", views.CarCreateView.as_view(), name = "car-create"),
+    path("list_car/", views.CarListView.as_view(), name = "car-list"),
+    path("detail_car/<int:pk>", views.CarDetailView.as_view(), name = "car-detail"),
+    path("update_car/<int:pk>", views.CarUpdateView.as_view(), name = "car-update"),
+    path("delete_car/<int:pk>", views.CarDeleteView.as_view(), name = "car-delete"),
+]
