7056dd4c1548851191f1d7439d2ebafad1a8324f
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-04-27 01:05:17 +0900

    [ADD-MODIFY] modify: list view added anchor tags, add: detail view and page, cart view and page -> cart is not yet

--- backend/Opendata/web/views.py
+++ backend/Opendata/web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView,  CreateView, UpdateView, DeleteView
@@ -6,13 +6,12 @@ from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 
-from .models import SeoulData
+from .models import SeoulData, Datacart
 from django.utils import timezone
 
-# Create your views here.
 class MainView(TemplateView) :
     template_name = "web/main.html"
-    
+
     def get_context_data(self, **kwargs: str) -> dict[str, str]:
         context =  super().get_context_data(**kwargs)
         context["name"] = "My project"
@@ -30,3 +29,23 @@ class OpenListView(ListView) :
 
 class OpenDetailView(DetailView) :
     model = SeoulData
+
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+
+class CartView(TemplateView):
+    template_name = 'web/datacart.html'
+
+    def get(self, request, *args, **kwargs):
+        cart_data = request.session.get('cart', {})
+        context = {'cart_data': cart_data}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        seoul_data_id = request.POST.get('seoul_data_id')
+        if seoul_data_id:
+            cart_data = request.session.get('cart', {})
+            cart_data[seoul_data_id] = cart_data.get(seoul_data_id, 0) + 1
+            request.session['cart'] = cart_data
+        return redirect('cart')
+
