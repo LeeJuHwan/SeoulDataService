458b7e3a8a5c91b34294431246fd9dfa2f565df8
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-04-29 17:33:48 +0900

    [modify] generic view to view. mvt pattern of template control only one template

--- backend/Opendata/web/urls.py
+++ backend/Opendata/web/urls.py
@@ -6,8 +6,9 @@ app_name = "web"
 urlpatterns = [
     path("", views.MainView.as_view(), name = "main"),
     path("cart/", views.CartView.as_view(), name = "cart"),
-    path("cart/add/<int:pk>", views.AddToCartView.as_view(), name = "create-page"),
-    path("list/", views.OpenListView.as_view(), name = "list-page"),
+    # path("cart/add/<int:pk>", views.AddToCartView.as_view(), name = "create-page"),
+    # path("list/", views.OpenListView.as_view(), name = "list-page"),
+    path("list/", views.OpenDataView.as_view(), name = "list-page"),
     path("detail/<int:pk>", views.OpenDetailView.as_view(), name = "detail-page"),
     path("delete/<int:pk>", views.RemoveFromCartView.as_view(), name = "remove_from_cart"),
 ]
--- backend/Opendata/web/views.py
+++ backend/Opendata/web/views.py
@@ -10,21 +10,86 @@ from django.utils import timezone
 
 class MainView(TemplateView) :
     template_name = "web/main.html"
-
+    
     def get_context_data(self, **kwargs: str) -> dict[str, str]:
         context =  super().get_context_data(**kwargs)
         context["name"] = "My project"
         return context
 
-class OpenListView(ListView) :
-    model = SeoulData
-    paginate_by = 100
+class OpenDataView(View):
+    """Project data list and cart product."""
+    template_name = "web/seouldata_list.html"
 
-    queryset = SeoulData.objects.all()
-    def get_context_data(self, **kwargs) : 
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
+    def get(self, request):
+        """Connect urls."""
+        # del request.session['cart_items']
+        seouldata_queryset = SeoulData.objects.all()
+        cart_items = self.request.session.get('cart_items', {})
+        print("cart_items", cart_items)
+        datacart_items = []
+        for product_id, _ in cart_items.items():
+            product = SeoulData.objects.get(id=product_id)
+            datacart_items.append({
+                'service_id': product.서비스ID,
+                'product': product.서비스명,
+                'category': product.소분류,
+                "product_id": product_id
+                })
+        context = {"seouldata_list" : seouldata_queryset,
+                    "datacart_list" : datacart_items}
+        
+        return render(request, self.template_name, context)
+
+        
+        # items = []
+        # for product_id, quantity in cart_items.items():
+        #     product = SeoulData.objects.get(id=product_id)
+        #     items.append({
+                # 'service_id': product.서비스ID,
+                # 'product': product.서비스명,
+                # 'category': product.소분류,
+                # "product_id": product_id
+        #     })
+        # context['items'] = items
+
+    def post(self, request):
+        """Submit data."""
+        cart_items = self.request.session.get('cart_items', {})
+        id_list = self.request.POST.getlist("selected_items")
+        print(id_list)
+        for product_id in id_list:
+            cart_items[product_id] = cart_items.get(product_id, 1)
+        self.request.session['cart_items'] = cart_items  
+
+        queryset = SeoulData.objects.all()
+        context = {"object_list": queryset, "cart_items": cart_items}
+        # print("####", self.request.session['cart_items'])
+        return render(request, self.template_name, context)
+
+      
+
+        # cart = request.session.get('cart', [])
+        # selected_items = request.POST.getlist('selected_items')
+        # print("selected_item : ", selected_items)
+        # for item_id in selected_items:
+        #     item = SeoulData.objects.get(pk=item_id)
+        #     cart.append({
+        #         'service_id': item.서비스ID,
+        #         'product': item.서비스명,
+        #         'category': item.소분류,
+        #         "product_id": item_id})
+        # request.session['cart'] = cart
+
+
+# class OpenListView(ListView) :
+#     model = SeoulData
+#     paginate_by = 100
+
+#     queryset = SeoulData.objects.all()
+#     def get_context_data(self, **kwargs) : 
+#         context = super().get_context_data(**kwargs)
+#         context["now"] = timezone.now()
+#         return context
 
 class OpenDetailView(DetailView) :
     model = SeoulData
@@ -37,9 +102,7 @@ class CartView(TemplateView):
         cart_items = self.request.session.get('cart_items', {})
         items = []
         for product_id, quantity in cart_items.items():
-            # print("##", product_id)
             product = SeoulData.objects.get(id=product_id)
-            # print("########",product)
             items.append({
                 'service_id': product.서비스ID,
                 'product': product.서비스명,
@@ -49,13 +112,13 @@ class CartView(TemplateView):
         context['items'] = items
         return context
 
-class AddToCartView(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        product_id = self.kwargs.get('pk')
-        cart_items = self.request.session.get('cart_items', {})
-        cart_items[product_id] = cart_items.get(product_id, 0) + 1
-        self.request.session['cart_items'] = cart_items
-        return '/web/cart/'
+# class AddToCartView(RedirectView):
+#     def get_redirect_url(self, *args, **kwargs):
+#         product_id = self.kwargs.get('pk')
+#         cart_items = self.request.session.get('cart_items', {})
+#         cart_items[product_id] = cart_items.get(product_id, 0) + 1
+#         self.request.session['cart_items'] = cart_items
+#         return '/web/cart/'
 
 class RemoveFromCartView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
@@ -67,4 +130,4 @@ class RemoveFromCartView(RedirectView):
             if cart_items[product_id] == 0:
                 del cart_items[product_id]
         self.request.session['cart_items'] = cart_items
-        return '/web/cart/'
+        return "/web/list/"
