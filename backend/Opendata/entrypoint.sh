818e1e1585df7ec01391173f40dcf8aa62d28035
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-05-01 15:06:36 +0900

    [modify] docker-compose yml file in multiple shell comands change shell script file

---
+++ backend/Opendata/entrypoint.sh
@@ -0,0 +1,10 @@
+set +e 
+echo "==> Django setup, executing: makemigrations "
+python3 manage.py makemigrations
+echo "==> Django setup, executing: migrate pro"
+python3 manage.py migrate
+echo "==> Django setup, executing: bulk create data"
+python3 manage.py db_uploader
+echo "==> Django Run Server"
+python3 manage.py runserver 0.0.0.0:8000
+
