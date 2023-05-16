99fb50458a7363d5dc3f44c652d8ccb8c1044337
kanggeowon <kangjiwon5961@gmail.com>
2023-05-16 22:37:52 +0900

    [ADD] connect front&back update

--- backend/Opendata/static_root/javascript/main.js
+++ backend/Opendata/static_root/javascript/main.js
@@ -19,8 +19,6 @@ gData.links.forEach(link => {
 
 // <div class = "graph"> 에 3D graph 생성
 const graph_elem = document.querySelector(".graph")
-
-// const Graph = ForceGraph3D()(graph_elem)
 const Graph = ForceGraph3D()(graph_elem)
 .jsonUrl("/web/node-coordinate/") 
 .nodeLabel("name") // hover print
@@ -34,15 +32,17 @@ const Graph = ForceGraph3D()(graph_elem)
 });
 
 // 데이터 정보 div 선택
-var data_info_name = document.querySelector(".data_info > .content > span:nth-child(1) > .val")
-var data_info_description = document.querySelector(".data_info > .content > span:nth-child(2) > .val")
-var data_info_subdivision = document.querySelector(".data_info > .content > span:nth-child(3) > .val")
-var data_info_type = document.querySelector(".data_info > .content > span:nth-child(4) > .val")
-var data_info_URL = document.querySelector(".data_info > .content > span:nth-child(5) > .val")
+var data_info_name = document.querySelector(".data_info .content_list > li:nth-child(1) > .val")
+var data_info_description = document.querySelector(".data_info .content_list > li:nth-child(2) > .val")
+var data_info_subdivision = document.querySelector(".data_info .content_list > li:nth-child(3) > .val")
+var data_info_type = document.querySelector(".data_info .content_list > li:nth-child(4) > .val")
+var data_info_URL = document.querySelector(".data_info .content_list > li:nth-child(5) > .val")
 
 
 // 유사 데이터 div 선택
-var similar_data_content = document.querySelector(".similar_data > .content")
+var similar_data_content = document.querySelector(
+  ".similar_data .similar_list"
+);
 
 window.onload = function(){
     console.log(" window.onload | page 들어오면 실행되는 함수: ", gData)
@@ -62,21 +62,25 @@ window.onload = function(){
 
 function select_node(node){
     console.log("function select_node | node 선택 : ", node)
-    
     Checked(node)
     // get_adjacent_data(node)
 
     const distance = 400;
-    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
 
-    const newPos = node.x || node.y || node.z
-        ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
-        : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+    const newPos =
+        node.x || node.y || node.z
+            ? {
+                  x: node.x * distRatio,
+                  y: node.y * distRatio,
+                  z: node.z * distRatio,
+              }
+            : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
 
     Graph.cameraPosition(
         newPos, // new position
         node, // lookAt ({ x, y, z })
-        1500  // ms transition duration
+        1500 // ms transition duration
     );
 }
 
@@ -131,34 +135,139 @@ function Checked(node) {
       .catch((error) => console.error(error));
   }
 
+// 드래그 앤 드랍 코드
+function dragElement(elmnt, target_elemnt) {
+  let pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
 
-function get_adjacent_data(node){
-    // 유사한 데이터 id 출력
-    similar_data_content.innerHTML = ""
-    for(let node_idx of node.adjacent_nodes){
-        let adj_span = document.createElement("span")
-        
-        let textnode = document.createTextNode(node_idx)
-        adj_span.appendChild(textnode)
-        
-        adj_span.classList.add("adj_data")
-        adj_span.addEventListener("click", (e) => {
-            search_node(node_idx)
-        })
-        similar_data_content.appendChild(adj_span)
-        
-        console.log("4. get_adjacent_data | 유사한 data id 출력:", textnode)
-    }
+  function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      target_elemnt.style.top = target_elemnt.offsetTop - 10 - pos2 + "px";
+      target_elemnt.style.left = target_elemnt.offsetLeft - 10 - pos1 + "px";
+  }
+
+  function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+  }
 }
 
-function search_node(idx){
-    // console.log("search_node")
-    try{
-        select_node(gData.nodes[idx])
-    }
-    catch{
-        console.log(`failed to search ${idx}`)
-    }
+////////
+(async function () {
+  jsonData = await load(); // load도 상수화,, json이 달라지니까
+  //const nodesById = Object.fromEntries(jsonData.nodes.map(node => [node.name, node]))
+      const getNodesTree = (nh) => {
+      const visibleNodes = []
+      const visibleLinks = []
+      jsonData.nodes.forEach(i => visibleNodes.push(i))
 
+      let arr = visibleNodes.filter(i => {
+          return i.collapsed == false
+      })
+      console.log('arr',arr)
+
+      if (!nh){
+          return { nodes: visibleNodes, links: visibleLinks};
+      }
+
+      if (!nh.collapsed) {
+          console.log('collapsed')
+          similar = similardata(nh)
+          similar.nodes.forEach(i => visibleNodes.push(i))
+          similar.links.forEach(i => visibleLinks.push(i))
+      }
+
+      if (arr){
+          arr.forEach(i => {
+              if (i != nh){
+              similar = similardata(i)
+              similar.nodes.forEach(i => visibleNodes.push(i))
+              similar.links.forEach(i => visibleLinks.push(i))
+          }})
+      }
+
+      console.log('visi',visibleNodes)
+      return { nodes: visibleNodes, links: visibleLinks};
+  }
+  Graph.graphData(getNodesTree());
+  Graph.onNodeClick((node) => {
+      select_node(node);
+      bool = Object.keys(node).includes("category")
+      if (bool){
+          return ;
+        }else{
+          node.collapsed = !node.collapsed; // toggle collapse state
+        }
+      Graph.graphData(getNodesTree(node))
+
+      intoTheNode(node,Graph)
+
+  });
+  Graph.linkWidth(2)
+  let search = document.querySelector(".search_button");
+  search.addEventListener("click", (e) => {
+      let search_target = document.querySelector(".search").value;
+      search_node(parseInt(search_target));
+  });
+
+  let side_windows = document.querySelectorAll(".side_window");
+  for (let side_window of side_windows) {
+      dragElement(side_window.querySelector(".title"), side_window);
+      dragElement(side_window.querySelector("button"), side_window);
+  }
+
+  let side_window_buttons = document.querySelectorAll(
+      ".side_window > button"
+  );
+
+  for (let but of side_window_buttons) {
+      but.addEventListener("click", (e) => {
+          but.parentNode.classList.toggle("open");
+      });
+  }
+  })();
+
+function search_node(idx) {
+  try {
+      select_node(gData.nodes[idx]);
+  } catch {
+      console.log(`failed to search ${idx}`);
+  }
 }
+
+
+// 나중에 처리
+// function get_adjacent_data(node) {
+//     console.log('새로ㅓ운 부분',node)
+//     similar_data_content.innerHTML = "";
+//     for (let node_idx of node.adjacent_nodes) {
+//         let adj_span = document.createElement("li");
+
+//         let textnode = document.createTextNode(node_idx);
+//         adj_span.appendChild(textnode);
+
+//         adj_span.classList.add("adj_data");
+//         adj_span.addEventListener("click", (e) => {
+//             search_node(node_idx);
+//         });
+//         similar_data_content.appendChild(adj_span);
+//     }
+// }
 
