eaa836f71268f516f4bccf2a9829d5e7b504d146
TaekGwan Kim <sbe07032@naver.com>
2023-05-22 11:02:09 +0900

    [Modify] data2faiss.py

--- GPT/data2faiss.py
+++ GPT/data2faiss.py
@@ -1,12 +1,19 @@
 from faiss_index import FaissIndex
+import pickle
 
 if __name__ == '__main__':
-    index_file_path = 'Opendata/web/index.faiss'
-    data_file_path = 'Opendata/csv_file/서울시 공공데이터 최종_reg1.csv'
-    embeddings_path = 'Opendata/csv_file/embeddings.pkl'
+    label_path = 'Opendata/csv_file/label_dict.pkl'
+    with open(label_path, 'rb') as f:
+        label = pickle.load(f)
 
-    index = FaissIndex(index_file_path=index_file_path,
-                       data_file_path=data_file_path,
-                       embeddings_path=embeddings_path)
+    data_file_path = 'Opendata/csv_file/서울시 공공데이터 최종.csv'
 
-    index.build_index(id_Col='서비스ID', data_Col='서비스명')
+    for key, name in label.items():
+        index_file_path = f'Opendata/web/index_{key}.faiss'
+        embeddings_path = f'Opendata/csv_file/embeddings_{key}.pkl'
+        index = FaissIndex(index_file_path=index_file_path,
+                           data_file_path=data_file_path,
+                           embeddings_path=embeddings_path
+                           )
+        index.build_index(id_Col='서비스ID', data_Col='서비스명', group_name=name)
+        index.save_index()
